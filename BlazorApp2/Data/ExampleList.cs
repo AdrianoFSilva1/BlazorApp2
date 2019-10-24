b7eaf5b6bc020071b03a1312cb9c2060236bbdb5
AdrianoSilva16601 <AdrianoSilva16601@outlook.com>
2019-10-24 16:56:17 +0100

    Issue2

---
+++ BlazorApp2/Data/ExampleList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Data
+{
+    public class Example
+    {
+        public string Examples { get; set; }
+
+        public Example(string examples)
+        {
+            Examples = examples;
+        }
+    }
+}
